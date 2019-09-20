a931358240b94d2e046088cebbebf77077104b4b
SENIOR\jean.poiski <jean.poiski@senior.com.br>
2019-09-20 16:06:48 -0300

    Commit inicial

---
+++ estoquefacilapi/src/main/java/com/poiski/estoquefacilapi/EstoquefacilapiApplication.java
@@ -0,0 +1,13 @@
+package com.poiski.estoquefacilapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquefacilapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquefacilapiApplication.class, args);
+	}
+
+}
---
+++ estoquefacilapi/src/test/java/com/poiski/estoquefacilapi/EstoquefacilapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poiski.estoquefacilapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstoquefacilapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
